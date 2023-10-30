055a97320c6dbb16f2bebb2cb4c7891b960f8904
Sacilave <sacilave@gmail.com>
2023-10-30 09:14:33 +0800

    main update

--- main.c
+++ main.c
@@ -1,43 +1,19 @@
+#define BoardLimit 30
+
 #include <stdio.h>
 #include <Windows.h>
 #include <stdlib.h>
 
 char startTitle[] = "开始游戏";
 
-int layout() {
-	const int row = 3, column = 3; 
-	printf("输入棋盘的长和宽\n");
-	scanf("%d %d", &row, &column);
-
-
-	// 创建动态二维数组
-	char** board = (char**)malloc(sizeof(char*) * row);
-	for (int i = 0; i < row; i++) {
-		*(board + i) = (char*)malloc(sizeof(char) * column);
-	}
-	// 将数组重新赋值为空值
-	for (int i = 0; i < column; i++) {
-		free(*(board + i));
-		board[i] = ' ';
-	}
-
-	// 刷新棋盘
-	printf("\n----\n棋盘\n");
+int refreshBorad(int *board, int row, int column) {
+	printf("\n------\n棋盘\n");
 	for (int i = 0; i < row; i++)
 	{
-		for (int j = 0; j < column; j++)
-		{
-			printf("----");
-		}
-		printf("-");
-		printf("\n");
-		for (int j = 0; j < column; j++)
-		{
-			printf("| %c ", board[i, j]);
-		}
-		printf("|");
-		printf("\n");
-		
+		for (int j = 0; j < column; j++) { printf("----"); }
+		printf("-"); printf("\n");
+		for (int j = 0; j < column; j++) { printf("| %c ", *(board + i * column * i + j)); }  // 取值
+		printf("|"); printf("\n");
 	}
 	for (int j = 0; j < column; j++)
 	{
@@ -46,36 +22,34 @@ int layout() {
 	printf("-");
 }
 
-/*
-for (int i = 0; i < row; i++)
-	{
-		for (int j = 0; j < column; j++)
-		{
-			board[i][j] = ' ';
-		}
+int initBoard() {
+	system("cls");
+	int row = 3, column = 3; char board[BoardLimit][BoardLimit];
+	for (int i = 0; i < BoardLimit; i++) { for (int j = 0; j < BoardLimit; j++) { board[i][j] = ' '; } }  // 初始赋值为 ' '
+	printf("输入棋盘的长和宽\n");
+	scanf("%d %d", &row, &column);
+	if (row > BoardLimit || column > BoardLimit) 
+	{ 
+		printf("长和宽都应小于%d\n", BoardLimit); 
+		Sleep(1000);
+		initBoard();
 	}
-
-释放动态二维数组内存
-for (i = 0; i < m; i++) {
-	free(*(aa + i));
-	aa[i] = NULL;
+	else for (int i = 0; i < row; i++) { for (int j = 0; j < column; j++) { board[i][j] = ' '; } }
+	refreshBorad(board, row, column);
 }
-free(aa);
-aa = NULL;
-*/
 
-int title() {
+int title() {  // 标题界面，会调用 layout()
 	while (1)
 	{
-		char input = getchar();
 		printf("\n------------ 五子棋 -------------\n1.%s\t\t2.退出\n", &startTitle);
+		char input = getchar();
 		system("cls");
-		if (input == '1') { layout(); break; }
+		if (input == '1') { initBoard(); break; }
 		else if (input == '2') { printf("退出中...\n"); exit(0); break; }
 		else { printf("输入错误！不就两个选项嘛？！\n"); continue; }
 	}
 }
 
-int main() {
-	layout();
+int main22() {
+	initBoard();
 }
